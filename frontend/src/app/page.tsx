560ebe7da4ca03b2b0092aa8a256ee270c2360cd
Jorge Gallardo <jorgito899@gmail.com>
2024-12-16 14:48:45 -0300

    Agregar funcionalidad para guardar tiempos en el cronómetro al detenerlo

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 export default function Home() {
   const [time, setTime] = useState(0);
   const [isRunning, setIsRunning] = useState(false);
+  const [savedTimes, setSavedTimes] = useState<number[]>([]);
   const startTimeRef = useRef<number | null>(null);
   const animationFrameRef = useRef<number | null>(null);
 
@@ -25,6 +26,10 @@ export default function Home() {
 
   const handleKeyPress = (event: KeyboardEvent) => {
     if (event.code === "Space") {
+      if (isRunning) {
+        setSavedTimes((prevSavedTimes) => [...prevSavedTimes, time]);
+        setTime(0);
+      }
       setIsRunning((prevIsRunning) => !prevIsRunning);
     }
   };
@@ -34,7 +39,7 @@ export default function Home() {
     return () => {
       window.removeEventListener("keydown", handleKeyPress);
     };
-  }, []);
+  }, [isRunning, time]);
 
   // Formatear el tiempo (minutos:segundos:milisegundos)
   const formatTime = (time: number) => {
@@ -46,18 +51,31 @@ export default function Home() {
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      {/* Cronómetro que comienza en cero y que empieza y detiene cuando presionamos la tecla espacio */}
       <div className="grid grid-cols-1 gap-4">
         <div className="grid grid-cols-1 gap-4">
           <div className="text-4xl text-center">{formatTime(time)}</div>
           <div className="text-center">
             <button
               className="px-4 py-2 bg-blue-500 text-white rounded-md"
-              onClick={() => setIsRunning((prevIsRunning) => !prevIsRunning)}
+              onClick={() => {
+                if (isRunning) {
+                  setSavedTimes((prevSavedTimes) => [...prevSavedTimes, time]);
+                  setTime(0);
+                }
+                setIsRunning((prevIsRunning) => !prevIsRunning);
+              }}
             >
               {isRunning ? "Stop" : "Start"}
             </button>
           </div>
+        </div>
+        <div className="text-left">
+          <h2 className="text-2xl">Tiempos guardados:</h2>
+          <ul>
+            {savedTimes.map((savedTime, index) => (
+              <li key={index}>{formatTime(savedTime)}</li>
+            ))}
+          </ul>
         </div>
       </div>
     </div>
